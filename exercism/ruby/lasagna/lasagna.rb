9618fc54c92f7d483fd75a56c095239c9874e271
Wilian <wiliancirillo2@gmail.com>
2022-04-15 21:49:11 -0300

    Nova resolução .1 para o  Exercism 'lasagna'

--- exercism/ruby/lasagna/lasagna.rb
+++ exercism/ruby/lasagna/lasagna.rb
@@ -3,11 +3,11 @@ class Lasagna
   TIME_PER_LAYER = 2
   
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    return Lasagna::EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+    return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
  
   def preparation_time_in_minutes(layers)
-    return layers * TIME_PER_LAYER
+    return TIME_PER_LAYER * layers
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
